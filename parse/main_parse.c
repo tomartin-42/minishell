7f78ed2e047594e7101c3dd576a41031459dc2b9
Tommy <tommy@localhost.localdomain>
2021-09-24 19:42:52 +0200

    refactoring main_parse

--- parse/main_parse.c
+++ parse/main_parse.c
@@ -6,7 +6,7 @@
 /*   By: tommy <tommy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/24 15:12:24 by tommy             #+#    #+#             */
-/*   Updated: 2021/09/24 18:46:43 by tommy            ###   ########.fr       */
+/*   Updated: 2021/09/24 19:40:38 by tommy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,17 +52,7 @@ void	main_parse(char *str, t_element *element)
 	i = 0;
 	j = 0;
 	while (str[i])
-	{
-		if ( mark_d == false && mark_s == false && str[i] == '"')
-		{
-			mark_d = true;
-			i++;
-		}
-		if (mark_s == false && mark_d == false && str[i] == 39)
-		{	
-			mark_s = true;
-			i++;
-		}		
+	{	
 		if (mark_d == true && mark_s == false && str[i] == '"')
 		{
 			mark_d = false;
@@ -73,6 +63,17 @@ void	main_parse(char *str, t_element *element)
 			mark_s = false;
 			i++;
 		}
+		if ( mark_d == false && mark_s == false && str[i] == '"')
+		{
+			mark_d = true;
+			i++;
+		}
+		if (mark_s == false && mark_d == false && str[i] == 39)
+		{	
+			mark_s = true;
+			i++;
+		}		
+
 		if (ft_strchr("|<> ", str[i]) && mark_d == false && mark_s == false)
 		{	
 			if(ft_strchr("|<>", str[i]))
