38b05dda1a10d32ddad88f24d06d247749f2b6a9
Tommy <tommy@localhost.localdomain>
2021-10-15 11:08:16 +0200

    work in execut_cmd pipe

--- exec/rutine.c
+++ exec/rutine.c
@@ -35,8 +35,9 @@ static void	execut_cmd(char **cmd, char **env, t_command *command)
 
 	if (command->multi_cmd[0]->type == 'P')
 	{
-		dup2(command->multi_cmd[0]->p_fd[0], STDIN_FILENO);
+		printf("HOLA1\n");
 		close(command->multi_cmd[0]->p_fd[1]);
+		dup2(command->multi_cmd[0]->p_fd[0], STDIN_FILENO);
 	}
 	pid = fork();
 	if (pid == 0)
@@ -47,6 +48,7 @@ static void	execut_cmd(char **cmd, char **env, t_command *command)
 			exit(errno);
 			if (command->multi_cmd[1]->type == 'P')
 			{
+				printf("HOLA2\n");
 				dup2(command->multi_cmd[1]->p_fd[1], STDOUT_FILENO);
 				close(command->multi_cmd[1]->p_fd[0]);
 			}
@@ -55,9 +57,6 @@ static void	execut_cmd(char **cmd, char **env, t_command *command)
 	}
 	else
 	{
-		waitpid(-1, NULL, 0);
-		waitpid(-1, NULL, 0);
-		waitpid(-1, NULL, 0);
 	}
 }
 
@@ -68,7 +67,9 @@ static t_element *get_last_pipe(t_command *command)
 	aux_elem = command->multi_cmd[0];
 	aux_elem = aux_elem->next;
 	while (aux_elem && aux_elem->type != 'P')
+	{
 		aux_elem = aux_elem->next;
+	}
 	return (aux_elem);
 }
 
@@ -77,9 +78,9 @@ void	rutine_command(t_element *element, t_env *env, t_command *command)
 	start_hered(element);
 	while(command->multi_cmd[0])
 	{
+	/// EMPIEZA LA FIESTA ///////////////////////////////////////////////
 		command->multi_cmd[1] = get_last_pipe(command);
 		command->env = extract_all_env_list(env);
-	/// EMPIEZA LA FIESTA ///////////////////////////////////////////////777
 	
 		extract_cmd_and_arg(command);
 	////////////////
@@ -95,6 +96,9 @@ void	rutine_command(t_element *element, t_env *env, t_command *command)
 		execut_cmd(command->cmd->arg, command->env, command);//TO DO:filtrar con la lista y ver si comand is 'B'
 		command->multi_cmd[0] = command->multi_cmd[1];
 	}
+		waitpid(-1, NULL, 0);
+		waitpid(-1, NULL, 0);
+		waitpid(-1, NULL, 0);
 	dup2(command->fd_stdin, STDIN_FILENO);
 	dup2(command->fd_stdout, STDOUT_FILENO);
 	close(command->fd_stdin);
