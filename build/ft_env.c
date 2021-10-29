01ca4c9be7b986993910bda456edf864eac112e6
tomartin-42 <tomasmartinezcurz@gmail.com>
2021-10-29 11:48:37 +0200

    fixed unset

--- build/ft_env.c
+++ build/ft_env.c
@@ -6,7 +6,7 @@
 /*   By: dpuente- <dpuente-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/14 10:57:35 by dpuente-          #+#    #+#             */
-/*   Updated: 2021/10/25 11:01:37 by dpuente-         ###   ########.fr       */
+/*   Updated: 2021/10/29 10:15:59 by tomartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,23 +18,14 @@ int	ft_env(t_env *env)
 		return (-1);
 	while (env)
 	{
-		ft_putstr_fd(env->var[0], 1);
-		write (1, "=", 1);
-		ft_putstr_fd(env->var[1], 1);
-		write (1, "\n", 1);
+		if (env->visible == true)
+		{
+			ft_putstr_fd(env->var[0], 1);
+			write (1, "=", 1);
+			ft_putstr_fd(env->var[1], 1);
+			write (1, "\n", 1);
+		}
 		env = env->next;
 	}
-	/*int	pos;
-
-	pos = 0;
-	if (!envs)
-		return (-1);
-	while (envs[pos])
-	{
-		ft_putstr_fd(envs[pos], 1);
-		write (1, "\n", 1);
-		pos++;
-	}*/
-	//exit (0);
 	return (0);
 }
--- build/ft_unset.c
+++ build/ft_unset.c
@@ -6,13 +6,12 @@
 /*   By: tomartin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/24 16:26:09 by tomartin          #+#    #+#             */
-/*   Updated: 2021/10/24 18:14:10 by tomartin         ###   ########.fr       */
+/*   Updated: 2021/10/29 11:45:37 by tomartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "build.h"
 #include "errorlib.h"
-
 
 static void	dell_var_t_env(t_env *env, char *var)
 {
@@ -24,6 +23,34 @@ static void	dell_var_t_env(t_env *env, char *var)
 		if (ft_strcmp(p_env->var[0], var) == 0)
 			p_env->visible = false;
 		p_env = p_env->next;
+	}
+}
+
+static void	clean_up_env_list(t_env *env)
+{
+	t_env	*p_env;
+	t_env	*aux;
+	t_env	*prev;
+
+	p_env = env;
+	prev = p_env;
+	while (p_env)
+	{
+		if (p_env->visible == false)
+		{
+			aux = p_env;
+			prev->next = aux->next;
+			free (aux->v_env);
+			free (aux->var[0]);
+			free (aux->var[1]);
+			free (aux);
+			p_env = prev;
+		}
+		else
+		{
+			prev = p_env;
+			p_env = p_env->next;
+		}
 	}
 }
 
@@ -44,7 +71,7 @@ int	ft_unset(t_command *command, t_env *env)
 			dell_var_t_env(env, command->cmd->arg[i]);
 		i++;
 	}
-
+	clean_up_env_list(env);
 	return (ret);
 }
 
