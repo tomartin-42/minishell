b74eba453fed6d6060bc91a2a963c73d26ba254c
tomartin-42 <tomasmartinezcurz@gmail.com>
2021-11-13 13:56:59 +0100

    fixed redir when only build

--- src/signal.c
+++ src/signal.c
@@ -3,24 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   signal.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dpuente- <dpuente-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tomartin <tomartin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/20 10:14:23 by tomartin          #+#    #+#             */
-/*   Updated: 2021/11/11 14:03:34 by dpuente-         ###   ########.fr       */
+/*   Updated: 2021/11/13 13:56:04 by tomartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-//Ctrl + C
-/* static void	exit_proces(int sig)
-{
-	(void)sig;
-	write(1, "\n", 1);
-	rl_replace_line("", 0);
-	rl_on_new_line();
-	rl_redisplay();
-} */
 
 void	select_signal(void)
 {
@@ -28,56 +18,17 @@ void	select_signal(void)
 	signal(SIGQUIT, SIG_IGN);
 }
 
-/*void	exit_in_proces(int sig)
-{
-	(void)sig;
-	write(1, "\n", 1);
-	rl_replace_line("", 0);
-}*/
-
-/*static void	quit_in_proces(int sig)
-{
-	(void)sig;
-	printf("HOLA\n");
-	write(2, "Quit\n", 5);
-	//signal(SIGQUIT, SIG_DFL);
-}*/	
-
 void	signal_in_proces(void)
 {
 	signal(SIGINT, SIG_DFL);
 	signal(SIGQUIT, SIG_DFL);
 }
 
-/*static void	exit_in_hered(int sig)
-{
-	(void)sig;
-	g_state = 130;
-	exit (130);
-}*/
-
-/*static void	quit_in_hered(int sig)
-{
-	(void)sig;
-	rl_replace_line("", 0);
-	rl_on_new_line();
-	rl_redisplay();
-}*/
-
 void	signal_hered(void)
 {
 	signal(SIGINT, SIG_IGN);
 	signal(SIGQUIT, SIG_IGN);
 }
-
-/*static void	exit_build_hered(int sig)
-{
-	(void)sig;
-	rl_replace_line("\03", 0);
-	rl_on_new_line();
-	rl_redisplay();
-	g_state = 130;
-}*/
 
 void	signal_build_hered(void)
 {
